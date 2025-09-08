19c0a55d9bfb76a1475ac31650793a22b509962a
Nedithgar Amirka <150447520+nedithgar@users.noreply.github.com>
2025-09-08 23:50:04 +0800

    refactor: remove redundant Ed25519 PEM support test

--- Tests/SwiftKeyGenTests/FormatConversion/Ed25519PEMTest.swift
+++ Tests/SwiftKeyGenTests/FormatConversion/Ed25519PEMTest.swift
@@ -6,28 +6,6 @@ import Crypto
 @Suite("Ed25519 PEM Support Test")
 struct Ed25519PEMTest {
     
-    @Test("Check if Ed25519 supports PEM")
-    func testEd25519PEMSupport() throws {
-        // Generate an Ed25519 key
-        let privateKey = Curve25519.Signing.PrivateKey()
-        
-        // Check if it has PEM representation
-        // Let's see what properties/methods are available
-        print("Private key type: \(type(of: privateKey))")
-        
-        // Try to get PEM representation
-        // Note: This is just to check what's available
-        // If pemRepresentation exists, it would be a property or method
-        
-        // Let's also check the public key
-        let publicKey = privateKey.publicKey
-        print("Public key type: \(type(of: publicKey))")
-        
-        // For now, let's just check raw representation
-        print("Private key raw representation size: \(privateKey.rawRepresentation.count)")
-        print("Public key raw representation size: \(publicKey.rawRepresentation.count)")
-    }
-    
     @Test("Try parsing Ed25519 PEM")
     func testParseEd25519PEM() throws {
         // This is a real Ed25519 private key in PKCS8 PEM format
